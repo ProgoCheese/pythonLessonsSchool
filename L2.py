9be1451cb003fbe6af229b5a0190fd1ea541b380
Artemii100500 <Artemii100500@gmail.com>
2023-11-12 22:21:05 +0700

    owf

--- L2.py
+++ L2.py
@@ -7,8 +7,4 @@ for i in range(len(user_list)):
         ans=ans+' '+ check
     else:
         check = user_list[i]
-#for i in range(len(user_list)):
-#    if user_list[i] != check:
-#        check = user_list[i]
-#        ans=ans+' '+ check
 print(ans)

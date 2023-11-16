a0472f727fb96c21e21d72e71ebffe20e594c3a7
Artemii100500 <Artemii100500@gmail.com>
2023-11-16 13:15:28 +0700

    owf

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ ans = ""
 if len(user_list) >= 2:
     for i in range(len(user_list)-1):
         sum = int(user_list[i-1])+int(user_list[i+1])
-        ans = ans +" " + str(sum)
+        ans = ans + ' ' + str(sum)
     sum = int(user_list[0])+int(user_list[len(user_list)-2])
     print(ans,sum)
 else:
